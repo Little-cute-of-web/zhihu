05d7689edd67a0091ba53f93398fd1187146ffb0
unknown <whobeautiful@163.com>
2022-08-29 22:18:30 +0800

    '完成disabled的属性'

--- src/components/DropDownItem.vue
+++ src/components/DropDownItem.vue
@@ -1,25 +1,27 @@
 <template>
-<li class="dropdown-option" :class="{'is-disable':disabled}">
-  <slot></slot>
-</li>
+  <li class="dropdown-option" :class="{ 'is-disabled': disabled }">
+      <slot></slot>
+  </li>
 </template>
 
-<script lang='ts'>
-  import {defineComponent} from 'vue';
-  export default defineComponent({
-    props:{
-      disabled:{
-        type:Boolean,
-        default:false
-      }
-    }
-  })
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  props: {
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+  },
+});
 </script>
-<style lang='less' scoped>
-  .dropdown-option.is-disabled *{
-    color:#6c757d;
-    pointer-events:none;
-    background-color:transparent;
-    
-  }
+<style lang="less" scoped>
+:slotted(.is-disabled.dropdown-option *) {
+  color: #6c757d;
+  pointer-events: none;
+  background-color: transparent;
+}
+// .dropdown-item{
+//   color:#f00;
+// }
 </style>
--- src/components/GlobalHeader.vue
+++ src/components/GlobalHeader.vue
@@ -13,9 +13,9 @@
       <li class="list-inline-item">
         <!-- <a href="#" class="btn btn-outline-light my-2">你好，{{ user.name }}</a> -->
         <drop-down :title="`你好，${user.name}`">
-          <drop-down-item><a class="dropdown-item" href="#">新建文章</a></drop-down-item>
-          <drop-down-item><a class="dropdown-item" href="#">编辑资料</a></drop-down-item>
-          <drop-down-item><a class="dropdown-item" href="#">退出登录</a></drop-down-item>
+          <dropdown-item><a class="dropdown-item" href="#">新建文章</a></dropdown-item>
+          <dropdown-item disabled><a class="dropdown-item" href="#">编辑资料</a></dropdown-item>
+          <dropdown-item><a class="dropdown-item" href="#">退出登录</a></dropdown-item>
         </drop-down>
       </li>
     </ul>
@@ -27,7 +27,7 @@
 import { defineComponent, PropType } from "vue";
 //导入组件
 import DropDown from './DropDown.vue';
-import DropDownItem from './DropDownItem.vue';
+import DropdownItem from './DropdownItem.vue';
 export interface UserProps {
   isLogin: boolean;
   name?: string;
@@ -43,7 +43,7 @@ export default defineComponent({
   },
   components:{
     DropDown,
-    DropDownItem
+    DropdownItem
   }
 });
 </script>
