6d3749f550e7b4010e4c91ee359acd878b284d7b
unknown <whobeautiful@163.com>
2022-08-30 20:43:44 +0800

    '完成home

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,21 @@
 <template>
   <div class="container">
     <global-header :user="user"></global-header>
-    <home></home>
+    <!-- <home></home> -->
+    <router-link to="/home"></router-link>
+    <router-link to="/login"></router-link>
+    <router-view></router-view>
+    <footer class="text-center py-4 text-secondary bg-light mt-6">
+      <small>
+        <ul class="list-inline mb-0">
+          <li class="list-line-item">2020者专栏</li>
+          <li class="list-line-item">课程</li>
+          <li class="list-line-item">文档</li>
+          <li class="list-line-item">联系</li>
+          <li class="list-line-item">更多</li>
+        </ul>
+      </small>
+    </footer>
   </div>
 </template>
 <script lang="ts">
@@ -11,7 +25,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 //导入header组件
 import GlobalHeader ,{ UserProps } from "./components/GlobalHeader.vue";
 //导入home组件
-import Home from "./views/Home.vue";
+// import Home from "./views/Home.vue";
 //输入测试用户
 const testUser: UserProps = {
   isLogin: true,
@@ -27,7 +41,7 @@ export default defineComponent({
   },
   components: {
     GlobalHeader,
-    Home
+    // Home
   },
 });
 </script>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,20 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 
-
+const Home = ()=>import('../views/Home.vue');
+const Login = ()=>import('../views/Login.vue')
 const routes: Array<RouteRecordRaw> = [
+  {
+    path:'',
+    redirect:'/home',
+    component:Home,
+    children:[]
+  },{
+    path:'/home',
+    component:Home,
+  },{
+    path:'/login',
+    component:Login,
+  }
 ]
 
 const router = createRouter({
