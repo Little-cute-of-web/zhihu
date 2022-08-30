57279db323437f213dcabeb238672ef90b5b369b
unknown <whobeautiful@163.com>
2022-08-30 11:29:13 +0800

    '使用validateInput组件完成邮箱验证和密码验证'

--- src/App.vue
+++ src/App.vue
@@ -3,41 +3,50 @@
     <!-- <demo></demo> -->
     <!-- <demo2></demo2> -->
     <global-header :user="user"></global-header>
-    <form>
+    <form action="">
       <div class="mb-3">
-        <label for="exampleInputEmail1">Email address</label>
-        <input
+        <label class = "form-label">Email address</label>
+        <!-- <input
           v-model="emailRef.val"
           type="email"
           class="form-control"
           id="exampleInputEmail1"
           aria-describedby="emailHelp"
-          @click="validateEmail"
+          @blur="validateEmail"
           
-        />
-        <div class="form-text" v-if="emailRef.error">
+        /> -->
+        <validate-input :rules="emailRules" 
+        v-model="emailVal"
+        type="text"
+        placeholder="请输入邮箱地址"
+        ></validate-input>
+        <!-- <div class="form-text" v-if="emailRef.error">
           {{ emailRef.message }}
-        </div>
+        </div> -->
+        <!-- {{emailVal}} -->
       </div>
       <div class="mb-3">
-        <label for="exampleInputPassword1">Password</label>
-        <input
+        <label for="form-label">Password</label>
+        <!-- <input
           type="password"
           class="form-control"
           id="exampleInputPassword1"
-        />
+        /> -->
+        <validate-input v-model="passwordVal" :rules="passwordRules"></validate-input>
       </div>
     </form>
     <!-- <column-list :list="list"></column-list> -->
   </div>
 </template>
 <script lang="ts">
-import { defineComponent, reactive } from "vue";
+import { defineComponent, reactive,ref} from "vue";
 //引入外部bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
 //项目组件导入
 import { ColumnProps } from "./components/ColumnList.vue";
 import GlobalHeader, { UserProps } from "./components/GlobalHeader.vue";
+//输入框组件
+import ValidateInput,{ RulesProps } from './components/ValidateInput.vue';
 const testData: ColumnProps[] = [
   {
     id: 1,
@@ -65,6 +74,24 @@ const testUser: UserProps = {
 export default defineComponent({
   name: "App",
   setup() {
+    //测试: update:modelValue
+    const emailVal = ref('one')
+    const passwordVal = ref('')
+    const emailRules :RulesProps = [
+      {
+        type:'required',message:'电子邮箱不能为空'
+      },{
+        type:'email',message:'请输入正确格式'
+      }
+    ]
+    const passwordRules :RulesProps = [
+      {
+        type:'required',message:'密码不能为空'
+      },{
+        type:'password',message:'至少8位'
+      }
+    ]
+    //非validateInput组件email的逻辑
     const emailRef = reactive({
       val: "",
       error: false,
@@ -86,12 +113,17 @@ export default defineComponent({
       user: testUser,
       emailRef,
       validateEmail,
-      emailReg
+      emailReg,
+      emailRules,
+      emailVal,
+      passwordRules,
+      passwordVal
     };
   },
   components: {
     // ColumnList,
     GlobalHeader,
+    ValidateInput
   },
 });
 </script>
---
+++ src/components/ValidateInput.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="validate-input-container pb-3">
+    <input class="form-control"
+    :class="{'is-invalid':inputRef.error}"
+    :value="inputRef.val"
+    @blur="validateInput"
+    @input="updateValue"
+    v-bind="$attrs"
+    >
+    <span v-if="inputRef.error" class="invalid-feedback">{{inputRef.message}}</span>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent,reactive,PropType } from 'vue'
+//邮箱
+const emailReg =  /^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+interface RuleProp {
+  type:'required' | 'email' | 'password';
+  message:string;
+}
+export type RulesProps = RuleProp[];
+//密码
+const passwordReg =  /^[a-zA-Z][a-zA-Z0-9_]{4,15}$/;
+export default defineComponent({
+  props:{
+    rules: Array as PropType<RulesProps>,
+    modelValue:String
+  },
+  //禁止继承attrs
+  inheritAttrs:false,
+
+  setup (props,context) {
+    const inputRef  =reactive({
+      val:props.modelValue || '',
+      error:false,
+      message:''
+
+    })
+    const validateInput = ()=>{
+      if(props.rules){
+        const allPassed = props.rules.every(rule=>{
+          let passed = true
+          inputRef.message  =rule.message
+          switch (rule.type) {
+            case "required":
+              passed =(inputRef.val.trim()!=='')
+              break;
+            case 'email':
+              passed=emailReg.test(inputRef.val)
+              break;
+            case 'password':
+              passed=passwordReg.test(inputRef.val)
+              break;
+            default:
+              break;
+          }
+          // console.log(passed);
+          
+          return passed
+        })
+        inputRef.error  =!allPassed;
+      }
+    }
+    const updateValue=(e: Event)=>{
+      const targetValue = (e.target as HTMLInputElement).value;
+      inputRef.val = targetValue;
+      context.emit('update:modelValue',targetValue)
+    }
+
+    return {
+      inputRef,
+      validateInput,
+      updateValue
+    }
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
